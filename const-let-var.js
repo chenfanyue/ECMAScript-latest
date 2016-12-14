209126bb2d290908b6f6ce32252ae8fc1307be64
chenfanyue <nicegogogo@163.com>
2016-12-14 14:45:18 +0800

    Create const-let-var.js

---
+++ const-let-var.js
@@ -0,0 +1,9 @@
+const names = ['a','b','c'];
+names.push(['d','e']);
+
+let variable = 'value';
+variable = 'new-value';
+
+// var is kind of deprecated.
+var variable = 'value';
+variable = 'new-value';
