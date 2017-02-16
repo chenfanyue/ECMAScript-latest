84c86ecdba2bf04e7f407bbaca2bbefa8d9cf46e
chenfanyue <nicegogogo@163.com>
2017-02-16 19:26:04 +0800

    Create string-additions.js

---
+++ string-additions.js
@@ -0,0 +1,9 @@
+let title = 'helloworld';
+
+console.log(title.indexOf('he'));
+console.log(title.indexOf('she'));
+console.log(title.includes('wore'));
+
+// 'string'.startsWith()
+// 'string'.endsWith()
+// 'string'.repeat()
